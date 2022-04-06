7155e70bba4a3a6cec03459765165c68daf51dc6
NgoBao1010 <ngoduybao1010@gmail.com>
2022-04-06 11:19:43 +0700

    upload

--- module5/ss3_angular_overview/baitap/angular-calculator-app/src/app/calculator/calculator.component.ts
+++ module5/ss3_angular_overview/baitap/angular-calculator-app/src/app/calculator/calculator.component.ts
@@ -6,8 +6,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calculator.component.css']
 })
 export class CalculatorComponent implements OnInit {
+  number1: number;
+  number2: number;
+  operator;
+  result: number;
 
+  addition() {
+    this.operator = '+';
+  }
 
+  subtraction() {
+    this.operator = '-';
+  }
+
+  multiplication() {
+    this.operator = '*';
+  }
+
+  division() {
+    this.operator = '/';
+  }
+
+  calculator(){
+    switch (this.operator) {
+      case '+':
+        this.result = parseFloat(String(this.number1)) + parseFloat(String(this.number2))
+        break;
+      case '-':
+        this.result = parseFloat(String(this.number1)) - parseFloat(String(this.number2))
+        break;
+      case '*':
+        this.result = parseFloat(String(this.number1)) * parseFloat(String(this.number2))
+        break;
+      case '/':
+        this.result = parseFloat(String(this.number1)) / parseFloat(String(this.number2))
+        break;
+    }
+  }
   constructor() { }
 
   ngOnInit(): void {
